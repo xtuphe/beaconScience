7cd7a5d1dc6fa0ba12c681255cf7c187ce73024b
Xtuphe <xtuphe@live.com>
2018-01-29 17:26:27 +0800

    define models

--- BeaconScience/Utils/StringTransformer.swift
+++ BeaconScience/Utils/StringTransformer.swift
@@ -8,36 +8,67 @@
 
 import UIKit
 
-public enum ChatMessageTypes {
-    case ChatTypeMessage
-    case ChatTypeChoice
-    case ChatTypeAction
-    case ChatTypeCondition
+public enum ChatActionType : NSInteger{
+    case Property = 0
+    case Game
 }
 
-//open func typeClassifier(rawString:String) -> NSArray {
-//    let rawArray = (rawString as NSString).components(separatedBy: "\n")
-//    var index = 0
-//    for singleLine in rawArray {
-//        if (singleLine == nil || singleLine = ""){
-//            continue//过滤空行
-//        }
-//        
-//        index += 1
-//    }
-//    
-//}
+open class ActionModel : NSObject {
+    open var type : ChatActionType?
+    open var gameName : String?
+    open var mathModel : MathModel?
+    
+    public init(rawStr:String){
+        let prefix = rawStr.first
+        let content = String(rawStr.dropFirst())
+        if prefix == "0" {
+            self.type = ChatActionType.Property
+            self.mathModel = MathModel.init(rawStr: content)
+        } else if prefix == "1" {
+            self.type = ChatActionType.Game
+            self.gameName = content
+        }
+    }
+}
+
+open class ConditionModel : NSObject {
+    open var name : String?
+    open var theOperator : String?
+    open var value : String?
+    
+    public init(rawStr:String){
+        let operators = [">=", "<=", ">", "<", "!=", "="]
+        for oprtr in operators {
+            if rawStr.contains(oprtr) {
+                self.theOperator = oprtr
+                var rawArray = (rawStr as NSString).components(separatedBy: oprtr)
+                self.name = rawArray[0]
+                self.value = rawArray[1]
+                break
+            }
+        }
+    }
+}
+
+open class MathModel : NSObject {
+    open var name : String?
+    open var theOperator : String?
+    open var value : String?
+    
+    public init(rawStr:String){
+        let operators = ["+", "-", "*", "/", "%"]
+        for oprtr in operators {
+            if rawStr.contains(oprtr) {
+                self.theOperator = oprtr
+                var rawArray = (rawStr as NSString).components(separatedBy: oprtr)
+                self.name = rawArray[0]
+                self.value = rawArray[1]
+            }
+        }
+    }
+}
 
 /*
- b 标记
- x 选择
- d 动作
- l 类型
- t 条件
- t 跳转
- s 时间
- j 间隔
- 
  m mark     标记
  a action   动作
  c condition条件
@@ -47,16 +78,60 @@ public enum ChatMessageTypes {
  */
 
 open class MessageModel: NSObject {
-//    open var index : NSInteger
-//    open var content : String
-//    open var choices : NSArray
-//    open var date : TimeInterval
-//    open var actions : NSArray
+    open var index : NSInteger?
+    open var mark : NSInteger?
+    open var content : String?
+    open var choice : Bool?
+    open var gap : TimeInterval?
+    open var actions : Array<ActionModel>?
+    open var conditions : Array<ConditionModel>?
+    open var date : String?
+    open var jump : NSInteger?
     
-    public init(string:String) {
-//        var separatedStrs = "";
-        
+    public init(rawStr:String, index: NSInteger) {
+        self.index = index
+        var targetStr = rawStr
+        if rawStr.hasPrefix("* ") {
+            self.choice = true
+            targetStr = String(rawStr.dropFirst(2))
+        }
+        var rawArray = (targetStr as NSString).components(separatedBy: "__")
+        self.content = rawArray[0]
+        rawArray.removeFirst()
+        for singleLine in rawArray {
+            let prefix = singleLine.first
+            let surfix = String(singleLine.dropFirst())
+            if prefix == "a" {
+                let action = ActionModel.init(rawStr: surfix)
+                self.actions?.append(action)
+            } else if prefix == "c" {
+                let condition = ConditionModel.init(rawStr: surfix)
+                self.conditions?.append(condition)
+            } else if prefix == "d" {
+                self.date = surfix
+            } else if prefix == "g" {
+                self.gap = (surfix as NSString).doubleValue
+            } else if prefix == "j" {
+                self.jump = (surfix as NSString).integerValue
+            } else if prefix == "m" {
+                self.mark = (surfix as NSString).integerValue
+            }
+        }
     }
 }
 
+public func transformModel(rawString:String) -> Array<MessageModel> {
+    let rawArray = (rawString as NSString).components(separatedBy: "\n")
+    var index = 0
+    var resultArray = Array<MessageModel>()
+    for singleLine in rawArray {
+        if (singleLine == ""){
+            continue//过滤空行
+        }
+        let model = MessageModel.init(rawStr: singleLine, index: index)
+        resultArray.append(model)
+        index += 1
+    }
+    return resultArray
+}
 
