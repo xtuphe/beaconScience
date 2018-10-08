b5a3f03b51e24bfbd2fda30fcdf30acd47fc5a9a
Xtuphe <xtuphe@live.com>
2018-10-08 14:52:41 +0800

    story

---
+++ BeaconScience/MiniGames/EnrollmentVC.swift
@@ -0,0 +1,30 @@
+//
+//  EnrollmentVC.swift
+//  BeaconScience
+//
+//  Created by Xtuphe on 2018/10/5.
+//  Copyright © 2018 Xtuphe's. All rights reserved.
+//
+
+import UIKit
+
+class EnrollmentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
